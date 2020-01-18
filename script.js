15c87ed88ce9a6770bb4ae8407701a2f13530a29
Nicholas Finch <nicfinch223@gmail.com>
2020-01-18 15:54:12 -0500

    final changes

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 var plength = prompt('How long would you like your password to be? (8-126)');
 
+let newPass = '';
+
+
 function generate() {
-    var passlength = plength;
 
     var values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+';
+    
 
-        let newPass = '';
-
-        for(var i = 0; i <= passlength; i++) {
-            newPass = newPass + values.charAt(Math.floor(Math.random() + Math.floor(values.length - 1))); 
-        }
+    for(var i = 0, n = values.length; i < plength; i++) {
+        newPass = newPass + values.charAt(Math.floor(Math.random() * n)); 
+    }   
+    return newPass;
 }
 
 document.getElementById('generateBtn').addEventListener('click', function(e) {
     e.preventDefault();
     generate();
-    document.querySelector('textarea').textContent = newPass;
+    document.querySelector('#textarea').textContent = newPass;
   });
+
+  function copyPassword(){
+    document.getElementById('copy').addEventListener('click', function() {
+    document.getElementById("password").select();
+    document.execCommand("Copy");
+    alert("Password copied to clipboard!");
+    });
+}; 
