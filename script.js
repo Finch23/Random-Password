f431504158f9f98603467271ca3eae76a0e19ad0
Nicholas Finch <nicfinch223@gmail.com>
2020-06-17 15:02:49 -0400

    final touches, might add clear function

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-
-
+const range = document.getElementById('myRange');
+let number = document.getElementById('myNumber');
 let newPass = '';
 
+range.addEventListener('input', sync)
+number.addEventListener('input', sync)
+
+function sync(e) {
+    const value = e.target.value
+    range.value = value
+    number.value = value
+}
 
 function generate() {
 
+    const length = number.value
     var values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-+';
     
 
-    for(var i = 0, n = values.length; i < plength; i++) {
+    for(var i = 0, n = values.length; i < length; i++) {
         newPass = newPass + values.charAt(Math.floor(Math.random() * n)); 
     }   
     return newPass;
